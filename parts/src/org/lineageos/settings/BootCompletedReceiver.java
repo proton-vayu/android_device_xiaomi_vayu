2a529725a2f913490d23808b6bad1ea4ef2fdaa0
kleidione Freitas <kleidione@gmail.com>
2021-03-24 16:55:11 -0300

    vayu:pats: Drop doze resources
    Signed-off-by: kleidione <kleidione@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,7 +24,6 @@ import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -40,7 +39,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.d(TAG, "Dirac is not present in system");
         }
-        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
 }
