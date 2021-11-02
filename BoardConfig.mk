386e9c0d27f3b523e403f007190ed3fa79fe67ee
orges <me@orgesified.ml>
2021-11-02 19:06:25 +0000

    vayu: adapt kernel make flags
    Signed-off-by: orges <me@orgesified.ml>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,10 +87,13 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CONFIG := vayu_user_defconfig
-  TARGET_KERNEL_CLANG_COMPILE := true
+  TARGET_KERNEL_CONFIG := vayu_defconfig
   TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
-  TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+  TARGET_KERNEL_VERSION := 4.14
+  KERNEL_LLVM_SUPPORT := false
+  KERNEL_GCC_TOOLCHAIN := aarch64-elf
+  KERNEL_GCC_ARM32_TOOLCHAIN := arm-eabi
+  TARGET_USES_UNCOMPRESSED_KERNEL := true
 endif
 
 # Partitions
