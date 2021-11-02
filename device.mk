a587971907701b81dc6bc5bf9120e1c20c7ca789
orges <me@orgesified.ml>
2021-11-02 19:06:43 +0000

    vayu: remove telephony-ext from boot jars
    Signed-off-by: orges <me@orgesified.ml>

--- device.mk
+++ device.mk
@@ -292,9 +292,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
