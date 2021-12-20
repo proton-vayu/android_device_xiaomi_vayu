cb217d62a0b1abab39fdef03aab41839bbe3dc89
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-20 09:21:01 +0700

    vayu: power: Add custom boost extension for scroll handling
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the vertical fling through device-specific
    boost override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside vertical scroll
    config inside perfboostsconfig.xml
    
    Reference: vendor/qcom/opensource/power/power-6150.c w/ reduced
    max interactive timeout

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,7 @@ BOARD_USES_METADATA_PARTITION := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
