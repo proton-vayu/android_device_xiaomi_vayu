57435b66f9f760ae0dc0633256302814ebfe7082
orges <me@orgesified.ml>
2022-02-07 22:18:56 +0000

    vayu: redfin february fp
    Signed-off-by: orges <me@orgesified.ml>

--- vayu.mk
+++ vayu.mk
@@ -19,4 +19,4 @@ PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys
